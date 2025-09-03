fd98e85c3f1a5a2205b97c5a18d857c9b546488d
Jon Nordby <jononor@gmail.com>
2025-09-03 22:52:06 +0200

    pdm: First iteration of C test program
    Compiles, but not tested

--- fpga/pico-ice/pdm_test/main.c
+++ fpga/pico-ice/pdm_test/main.c
@@ -2,52 +2,54 @@
 #include <gpio.h>
 #include <stdio.h>
 #include <csr.h>
+#include <uart.h>
 
-// VGA console base addess is 0x800_0340 (0x800_0000 + 0x40*13)
-volatile uint8_t* vga_text = (volatile uint8_t*)0x8000340;
+#define printf uart_printf
 
-volatile uint8_t* vga_bgcol = (volatile uint8_t*)0x8000370;
-volatile uint8_t* vga_tcol1 = (volatile uint8_t*)0x8000371;
-volatile uint8_t* vga_tcol2 = (volatile uint8_t*)0x8000372;
-volatile uint8_t* vga_status = (volatile uint8_t*)0x800037f;
 
-int count = 0;
+// PDM audio input
+// base address is 0x800_0280 (0x800_0000 + 0x40*10)
+volatile uint32_t* pdm_ctrl = (volatile uint32_t*)0x8000280;
+volatile uint32_t* pdm_clkp = (volatile uint32_t*)0x8000284;
+volatile uint32_t* pdm_pcmw = (volatile uint32_t*)0x8000288;
 
-void tqv_user_interrupt13(void) {
-  (void)*vga_status;  // Clear interrupt
+static int64_t sample_counter = 0;
 
-  char buf[11];
-  sprintf(buf, "%10d", count);
-  for (int i = 0; i < 10; ++i) vga_text[i] = buf[i];
+void tqv_user_interrupt10(void) {
 
-  if ((count & 0xf) == 0) {
-    const char scroll_text[] = "    TinyQV VGA Console!";
-    int text_len = sizeof(scroll_text) - 1;
-    int idx = (count >> 4) % text_len;
-    for (int i = 0; i < 10; ++i) {
-      vga_text[20+i] = scroll_text[idx];
-      if (++idx == text_len) idx = 0;
-    }
-  }
+    // Read sample. This also clears the interrupt
+    int32_t sample = *pdm_pcmw;
+    (void)sample; // TODO: put into a buffer
 
-  ++count;
+    sample_counter += 1;
 }
 
 int main(void) {
-  // Clear text
-  for (int i = 0; i < 30; ++i) vga_text[i] = ' ';
 
-  // Set all outputs to regular mode (not debug)
-  set_debug_sel(0xff);
+    // Set all outputs to regular mode (not debug)
+    set_debug_sel(0xff);
 
-  // Set all outputs to peripheral 13
-  for (int i = 0; i < 8; ++i) set_gpio_func(i, 13);
+    // Set all outputs to peripheral 10
+    for (int i = 0; i < 8; ++i) {
+        set_gpio_func(i, 10);
+    }
 
-  // Enable interrupt
-  enable_interrupt(13);
+    // Enable PDM clock
+    *pdm_clkp = 14;
 
-  // Loop forever - all further work will be done in the
-  // interrupt.
-  while(1);
-  return 0;
+    // Enable interrupt
+    enable_interrupt(10);
+    
+    static uint32_t previous_status = 0;
+    // Main loop
+    while (1) {
+        const uint32_t tick_us = read_time();
+
+        if ((tick_us - previous_status) > 1000000) {
+            printf("loop-status time_ms=%d sample_counter=%d", tick_us/1000, sample_counter);
+            previous_status = tick_us;
+        }
+    }
+
+    return 0;
 }
