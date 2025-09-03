6dce59e95cc0bb3deed5616661fe36a2a22e2c1d
Jon Nordby <jononor@gmail.com>
2025-09-03 22:27:13 +0200

    pico-ice: Also enable example in slot 15

--- fpga/pico-ice/peripherals.v
+++ fpga/pico-ice/peripherals.v
@@ -399,7 +399,7 @@ module tinyQV_peripherals #(parameter CLOCK_MHZ=64) (
         .user_interrupt(user_interrupts[14])
     );
 
-    tqvp_full_empty i_user_peri15 (
+    tqvp_full_example i_user_peri15 (
         .clk(clk),
         .rst_n(rst_n),
 
