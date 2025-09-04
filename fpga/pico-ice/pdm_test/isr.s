8fdc0c62488b032276a2ae7e5c85c9e22632de97
Martin Stensgård <mastensg@mastensg.net>
2025-09-04 23:59:56 +0200

    fpga/pico-ice/pdm_test: raw interrupt

---
+++ fpga/pico-ice/pdm_test/isr.s
@@ -0,0 +1,31 @@
+.section .text
+
+.extern buffer
+.extern buffer_idx
+
+.globl tqv_user_interrupt10_raw
+tqv_user_interrupt10_raw:
+	# out 4 hi
+	li	a0,	16
+	sw	a0,	64(tp)
+
+	# copy pcmw
+	la	s1,	buffer
+	la	a0,	buffer_idx
+	lw	a1,	0(a0)
+	add	s1,	s1,	a1
+	add	s1,	s1,	a1
+	addi	a1,	a1,	1
+	sh	a1,	0(s1)
+	andi	a1,	a1,	63
+	sw	a1,	0(a0)
+	lw	a0,	0x0288(tp)
+	sh	a1,	0(s1)
+
+	# out 4 lo
+	li	a0,	0
+	sw	a0,	64(tp)
+
+	# short_isr_exit
+	.2byte	0x3502
+	mret
--- fpga/pico-ice/pdm_test/main.c
+++ fpga/pico-ice/pdm_test/main.c
@@ -16,87 +16,51 @@ volatile uint32_t* pdm_pcmw = (volatile uint32_t*)0x8000288;
 volatile uint16_t* pdm_pcmw16 = (volatile uint16_t*)0x8000288;
 volatile uint8_t* pdm_pcmw8 = (volatile uint8_t*)0x8000288;
 
-// Storage of audio data
-#define BUFFER_LENGTH 128
-static int16_t buffer[BUFFER_LENGTH];
-uint32_t buffer_idx = 0;
-
-// Debug/profiling
-static uint32_t sample_counter = 0;
-static uint32_t interrupt_cycles = 0;
-
-void tqv_user_interrupt10(void) {
-
-    const uint32_t interrupt_start = read_cycle();
-
-    // Read sample. This also clears the interrupt
-    int16_t sample = *pdm_pcmw;
-    // uint8_t sample = *pdm_pcmw8;
-    // uint16_t sample = *pdm_pcmw16;
-    (void)sample; // TODO: put into a buffer
-
-#if 1
-    if (buffer_idx > BUFFER_LENGTH-1) {
-        buffer_idx = 0;
-    }    
-    buffer[buffer_idx++] = sample;
-#endif
-
-    sample_counter += 1;
-    // timing
-    interrupt_cycles = (read_cycle() - interrupt_start);
-}
-
-uint64_t compute_soundlevel() {
-
-    // non-optimize Root-Mean-Square (RMS)
-    uint64_t acc = 0;
-    // XXX: assumes that buffer is already filled
-    for (int i=0; i<BUFFER_LENGTH; i++) {
-        const int16_t sample = buffer[i];
-        //acc += (sample * sample);
-        acc += sample;
-    }
-    // sqrtf no symbol
-    //const float rms = sqrtf(acc / BUFFER_LENGTH);
-    const uint64_t mean = (acc / BUFFER_LENGTH);
-    return mean;
-}
+#define BUFFER_LENGTH 64
+volatile int16_t buffer[BUFFER_LENGTH];
+volatile uint32_t buffer_idx;
 
 int main(void) {
-
-    printf("main-start\n");
+    printf("main\n");
 
     // Set all outputs to regular mode (not debug)
     set_debug_sel(0xff);
 
+    // gpio 4
+    enable_all_outputs();
+
     // Set all output 6 to peripheral 10
     set_gpio_func(6, 10);
 
-    // Enable PDM clock
-    *pdm_clkp = 24; // FIXME: this should be 14
+    enable_interrupt(10);
+
+    *pdm_clkp = 14;
     *pdm_ctrl = 1;
 
-    // Enable interrupt
-    enable_interrupt(10);
-    
-    printf("loop-start\n");
+    printf("sampling...");
+    //while (!buffer[BUFFER_LENGTH-1])
+    while (buffer_idx < 60)
+    	    ;
+    printf("stopping...");
+    *pdm_ctrl = 0;
 
-    static uint32_t previous_status = 0;
-    // Main loop
+    printf("BEGIN\n");
+    for(int i=0; i<BUFFER_LENGTH; ++i)
+	    printf("%6d\n", buffer[i]);
+    printf("END\n");
+
+    for(;;)
+    	    ;
+    
+    uint32_t tick_ms_prev = 0;
     while (1) {
-        // Manually compensate for running on 14 Mhz instead of 64 Mhz
         const uint32_t tick_ms = read_time()*64/(14*1000);
 
-        if ((tick_ms - previous_status) > 3000) {
-            // NOTE: 3rd argument to printf not interpolated, just gives 0
-            //float sl = 0.0;
-            //sl = compute_soundlevel();
-            //sl = 33.11f;
-            uint64_t sl = compute_soundlevel();
-            printf("loop-status time_ms=%d cycles=%d rate=%d sound=%d \n",
-                tick_ms, interrupt_cycles, sample_counter/(tick_ms/1000), (int)sl);
-            previous_status = tick_ms;
+        if ((tick_ms - tick_ms_prev) > 1000) {
+            	for(int i=0; i<BUFFER_LENGTH; ++i)
+			printf("%6d\n", buffer[i]);
+		printf("\n");
+            tick_ms_prev = tick_ms;
         }
     }
 
